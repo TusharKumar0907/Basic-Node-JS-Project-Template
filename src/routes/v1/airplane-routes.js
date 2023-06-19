a9cfde9258df59ad4b902fd2a944f9e44fad8907
TusharKumar0907 <100371860+TusharKumar0907@users.noreply.github.com>
2023-06-20 03:32:18 +0530

    Update airplane-routes.js (added mildwares)

--- src/routes/v1/airplane-routes.js
+++ src/routes/v1/airplane-routes.js
@@ -1,12 +1,19 @@
 const express = require('express');
 
 const { AirplaneController } = require('../../controllers');
+const { AirplaneMiddleware } = require('../../middlewares');
 
 const router = express.Router();
 
 
 //  /api/v1/airplanes
-router.post('/', AirplaneController.CreateAirplane);
+
+// router.post('/', AirplaneController.CreateAirplane);
+
+//include the middlewares as well in order to check whether the request is valid or not
+router.post('/', AirplaneMiddleware.validateCreateRequest , AirplaneController.CreateAirplane);
+
+
 
 
 module.exports = router;
